9ee126c93d8648a896ae5b9f051efc553ac55f64
zizu <jimoheniola76@gmail.com>
2024-03-01 18:14:17 +0100

    complete navbar and footer

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+
+import './globals.css'
+import { Footer,Navbar } from '@/components'
+
+
+
+export const metadata = {
+  title: 'Car Hub',
+  description: 'Discover best cars',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className='relative'>
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  )
+}
---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+
+
+import { Hero } from '@/components'
+
+
+export default function Home() {
+  
+  return (
+    <main className="overflow-hidden">
+      <Hero /> 
+
+    </main>
+  )
+}
